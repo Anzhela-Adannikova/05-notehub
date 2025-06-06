18755128f5acfaa2a227c155d5aa49d25ba060f2
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-06-06 23:58:42 +0300

    add error component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,14 @@
 // контейнер додатка
 import { useState } from "react";
-import css from "./App.module.css";
 import { fetchNotes } from "../../services/noteService";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import css from "./App.module.css";
 import NoteList from "../NoteList/NoteList";
 import NoteForm from "../NoteForm/NoteForm";
 import NoteModal from "../NoteModal/NoteModal";
 import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,31 +16,33 @@ export default function App() {
   const [currentPage, setCurrentPage] = useState(0);
   const perPage = 12;
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["notes", currentPage, searchTerm],
     queryFn: () => fetchNotes(currentPage + 1, searchTerm, perPage),
     placeholderData: keepPreviousData,
   });
 
   const openModal = () => setIsModalOpen(true);
-
   const closeModal = () => setIsModalOpen(false);
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
+        <SearchBox onSearch={setSearchTerm} />
+        {data && data.totalPages > 1 && (
+          <Pagination
+            currentPage={currentPage}
+            pageCount={data.totalPages}
+            onPageChange={({ selected }) => setCurrentPage(selected)}
+          />
+        )}
         <button className={css.button} onClick={openModal}>
           Create note +
         </button>
-        <SearchBox onSearch={setSearchTerm} />
       </header>
       {isLoading && <strong className={css.loading}>Loading notes...</strong>}
-      {data && data.totalPages > 1 && (
-        <Pagination
-          currentPage={currentPage}
-          pageCount={data.totalPages}
-          onPageChange={({ selected }) => setCurrentPage(selected)}
-        />
+      {isError && searchTerm.trim() !== "" && (
+        <ErrorMessage message="Something went wrong. Please try again." />
       )}
       {data && <NoteList notes={data.notes} />}
       {isModalOpen && (
--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { useDebounce } from "use-debounce";
 import css from "./SearchBox.module.css";
+import toast from "react-hot-toast";
 
 interface SearchBoxProps {
   onSearch: (value: string) => void;
@@ -10,18 +11,31 @@ interface SearchBoxProps {
 export default function SearchBox({ onSearch }: SearchBoxProps) {
   const [inputValue, setInputValue] = useState("");
   const [debounceValue] = useDebounce(inputValue, 1000);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    onSearch(debounceValue.trim());
-  }, [debounceValue, onSearch]);
+    const trimmed = debounceValue.trim();
+    onSearch(trimmed);
+
+    if (trimmed === "" && inputValue !== "") {
+      setError("Try it once");
+      toast.error("Try it once");
+      return;
+    } else {
+      setError("");
+    }
+  }, [debounceValue, onSearch, inputValue]);
 
   return (
-    <input
-      className={css.input}
-      type="text"
-      placeholder="Search notes"
-      value={inputValue}
-      onChange={(event) => setInputValue(event.target.value)}
-    />
+    <>
+      <input
+        className={css.input}
+        type="text"
+        placeholder="Search notes"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      {error && <p className={css.error}>{error}</p>}
+    </>
   );
 }

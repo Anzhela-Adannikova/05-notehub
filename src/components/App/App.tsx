51255c918ecc2646045fb859e1e6e1b8ce5a1498
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-06-06 16:58:42 +0300

    add NoteForm

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 // контейнер додатка
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import css from "./App.module.css";
 import type { Note } from "../../types/note";
 import { fetchNotes, createNote, deleteNote } from "../../services/noteService";
 import { useQuery } from "@tanstack/react-query";
 import NoteList from "../NoteList/NoteList";
+import NoteForm from "../NoteForm/NoteForm";
 
 export default function App() {
   const { data, isLoading } = useQuery({
@@ -27,6 +28,13 @@ export default function App() {
         {/* Пагінація */}
         {/* Кнопка створення нотатки */}
       </header>
+      {isLoading && <strong className={css.loading}>Loading notes...</strong>}
+      {data && !isLoading && <NoteList notes={data} />}
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <NoteForm onSuccess={closeModal} />
+        </Modal>
+      )}
     </div>
   );
 }
--- src/components/NoteModal/NoteModal.tsx
+++ src/components/NoteModal/NoteModal.tsx
@@ -1,1 +1,14 @@
 // модальне вікно яке відкривається при створенні нотатки
+import css from "./NoteModal.module.css";
+
+interface NoteModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+export default function NoteModal({ onClose, children }: NoteModalProps) {
+  return (
+    <div className={css.backdrop} role="dialog" aria-modal="true">
+      <div className={css.modal}>{/* Компонент NoteForm */}</div>
+    </div>
+  );
+}
